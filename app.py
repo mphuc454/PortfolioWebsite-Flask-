e89894d0226baf7f833797a4714e86c02e39b487
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-03-11 18:31:54 +0700

    xu ly form

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
 import json
+import re
 import smtplib
 from email.mime.text import MIMEText
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
+# load skill từ file json
 def load_skills():
-    with open('skills.json', 'r', encoding='utf-8') as f:
+    with open('PythonPortfolio\\skills.json', 'r', encoding='utf-8') as f:
         my_data = json.load(f)
     return my_data['skills']
 
-@app.route('/home')
-def home():
-        skills = load_skills()
-        return render_template('index.html', skills = skills)
-@app.route('/skill')
-def skill():
-        return render_template('skill.html')
 
-# send email
+# xử lý send email
 def send_mail(from_email, message):
     SMTP_SERVER = "smtp.gmail.com"
     SMTP_PORT = 587
@@ -35,15 +30,28 @@ def send_mail(from_email, message):
         server.starttls()
         server.login(USERNAME, PASSWORD)
         server.sendmail(from_email, "22130215@st.hcmuaf.edu.vn", msg.as_string())
+        
+     
 @app.route('/home', methods = ['GET', 'POST'])
-def contact_form():
-    skills = load_skills()
-    if request.method == 'POST':
-        email = request.form['usremail']
-        message = request.form['mess']
+def home():
+        skills = load_skills()
+        error = None
+        if request.method == 'POST':
+            name = request.form['usrname']
+            email = request.form['usremail']
+            message = request.form['mess']
+            
+            #kiểm tra tính hợp lệ form:
+            if(len(name.strip()) < 2): 
+                 error = "Tên phải lớn hơn hoặc bằng 2 ký tự"
+            elif len(message) < 10:
+                error = 'Vui lòng nhập lời nhắn ít nhất 10 từ'
+            else:
+                send_mail(email, message)
+                return redirect(url_for('home'))
+                
+        return render_template('index.html', skills = skills, err = error)
 
-        send_mail(email, message)
-    return render_template("index.html", skills = skills)
 
 if __name__ == '__main__':
     app.run(debug=True)
